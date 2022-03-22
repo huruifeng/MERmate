f39df7a5baf6b00e333837c9a92a7c93eb4d4dff
RuifengHu <442490020@qq.com>
2022-03-22 00:12:46 -0400

    Init commit

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ from version import __version__
 import os
 
 def print_logo():
-    ##
+    ## http://www.patorjk.com/software/taag/#p=display&f=Crazy&t=MERmate
     # Use a breakpoint in the code line below to debug your script.
-    print("MERmate version %s" % __version__)
-    print("#######################################################")
-    print("   _____ _____ _____ _____ _____ _____ __ __           ")
-    print("  |  .  |   __| __  |   __|_  _ |   __|  |  |___ _ _   ")
-    print("  | | | |   __|    -|   __| | | |__   |     | . | | |  ")
-    print("  |_|_|_|_____|__|__|__|  |_____|_____|__|__|  _|_  |  ")
-    print("                                            |_| |__|  ")
-    print("#######################################################")
+    ver_str =" MERmate version: "+ __version__ + " "
+    print("#"* int(31-len(ver_str)/2) + ver_str + "#"*int(31-len(ver_str)/2))
+    print("    __  __   _____   ____                        _              ")
+    print("   |  \/  | | ____| |  _ \   _ __ ___     __ _  | |_    ___     ")
+    print("   | |\/| | |  _|   | |_) | | '_ ` _ \   / _` | | __|  / _ \\   ")
+    print("   | |  | | | |___  |  _ <  | | | | | | | (_| | | |_  |  __/    ")
+    print("   |_|  |_| |_____| |_| \_\ |_| |_| |_|  \__,_|  \__|  \___|    ")
+    print("01001101 01000101 01010010 01101101 01100001 01110100 01100101")
+    print("##############################################################")
 
 
 if __name__ == '__main__':
